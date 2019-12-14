483d8919ed0cd85ed27ffeb10a7ff6a7e456cec7
Alec Deason <alec@tinycountry.com>
2019-12-14 12:47:49 -0800

    Vein segment splitting

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use leaf_morphogenesis::{render::render, Leaf};
 fn main() {
     let mut leaf = Leaf::new();
     for _ in 0..10 {
-        leaf.step_simulation(1.0);
+        leaf.step_simulation(0.1);
     }
     let image = render(&leaf, 500, 500);
     image.write_png(args().nth(1).unwrap()).unwrap();
